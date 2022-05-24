385ded7383896444235f1fb821bedc6e5bb4e908
DanhHung123 <hungnguyena123ye@gmail.com>
2022-05-24 08:33:35 +0700

    up

--- Project_Java2/Main.java
+++ Project_Java2/Main.java
@@ -1,55 +1,73 @@
 package Project_Java2;
 
+import Coffee.Menu;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        Scanner scan = new Scanner(System.in);
-        List<Music> musicList = new ArrayList<>();
-        MusicList menu = new MusicList(musicList);
-        boolean check = true;
-        do {
-            System.out.println("===== MENU =====");
-            System.out.println("1. Them bai hat moi vao list");
-            System.out.println("2. Luu vào Database");
-            System.out.println("3. Chinh sua bai hat");
-            System.out.println("4. Xoa bai hat");
-            System.out.println("5. Tim kiem bai hat");
-            System.out.println("6. Hien thi list");
-            System.out.println("7. Hien thi Database");
-            System.out.println("8. Thoat");
-            int select = scan.nextInt();
-            switch (select) {
-                case 1:
-                    menu.AddMusic();
-                    break;
-                case 2:
-                    menu.SaveDatabase();
-                    break;
-                case 3:
-                    menu.UpdateMusic();
-                    break;
-                case 4:
-                    menu.RemoveMusic();
-                    break;
-                case 5:
-                    menu.SearchMusic();
-                    break;
-                case 6:
-                    menu.DisplayArrayList();
-                    break;
-                case 7:
-                    menu.DisplayDatabase();
-                    break;
-                case 8:
-                    check = false;
-                    break;
-                default:
-                    System.out.println("Lua chon sai !!!");
-                    break;
+        try(
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/MusicData", "root","");
+                Statement stmt = conn.createStatement();
+                PreparedStatement psmt = conn.prepareStatement("UPDATE Music set MusicName = ?, Author = ?, Years = ?, Types = ? where MusicId = ?");
+                PreparedStatement pstmt = conn.prepareStatement("Insert into Music values (?,?,?,?,?)")
+        ) {
+            try {
+                conn.setAutoCommit(false);
+                Scanner scan = new Scanner(System.in);
+                List<Music> musicList = new ArrayList<>();
+                MusicList menu = new MusicList(musicList);
+                menu.Synchronized(stmt);
+                boolean check = true;
+                do {
+                    System.out.println("===== MENU =====");
+                    System.out.println("1. Them bai hat moi vao list");
+                    System.out.println("2. Luu vao Database");
+                    System.out.println("3. Chinh sua bai hat");
+                    System.out.println("4. Xoa bai hat");
+                    System.out.println("5. Tim kiem bai hat");
+                    System.out.println("6. Hien thi list");
+                    System.out.println("7. Hien thi Database");
+                    System.out.println("8. Thoat");
+                    int select = scan.nextInt();
+                    switch (select) {
+                        case 1:
+                            menu.AddMusic();
+                            break;
+                        case 2:
+                            menu.SaveDatabase(pstmt,stmt);
+                            break;
+                        case 3:
+                            menu.UpdateMusic(psmt);
+                            break;
+                        case 4:
+                            menu.RemoveMusic(stmt);
+                            break;
+                        case 5:
+                            menu.SearchMusic(stmt);
+                            break;
+                        case 6:
+                            menu.DisplayArrayList();
+                            break;
+                        case 7:
+                            menu.DisplayDatabase(stmt);
+                            break;
+                        case 8:
+                            check = false;
+                            break;
+                        default:
+                            System.out.println("Lua chon sai !!!");
+                            break;
+                    }
+                }while (check);
+            }catch (SQLException ex){
+                ex.printStackTrace();
+                conn.rollback();
             }
-        }while (check);
+        }
     }
 }
--- Project_Java2/MusicList.java
+++ Project_Java2/MusicList.java
@@ -1,13 +1,14 @@
 package Project_Java2;
 
-
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MusicList {
     private List<Music> MusicList;
     private Scanner scan = new Scanner(System.in);
+    private List<Music> MusicList2 = new ArrayList<>();
 
     public MusicList(List<Music> MusicList) {
         this.MusicList = MusicList;
@@ -23,13 +24,22 @@ public class MusicList {
         }
         return check;
     }
-
+    public boolean checkId2(int id) {
+        boolean check = false;
+        for (Music m : MusicList2) {
+            if (m.getMusicId() == id) {
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
     public void AddMusic() {
         System.out.println("= Them bai hat =");
         System.out.println("Nhap ID :");
         int id = scan.nextInt();
         scan.nextLine();
-        if (checkId(id)) {
+        if (checkId(id) && checkId2(id)) {
             System.out.println("Bai hat da ton tai !!!");
         }else {
             System.out.println("Nhap ten bai hat: ");
@@ -40,55 +50,61 @@ public class MusicList {
             String year = scan.nextLine();
             System.out.println("Nhap the loai: ");
             String type = scan.nextLine();
-            MusicList.add(new Music(id,name,author,year,type));
+            MusicList2.add(new Music(id,name,author,year,type));
             System.out.println("Them thanh cong !");
         }
     }
 
-    public void UpdateMusic() throws Exception{
+    public void Synchronized(Statement stmt) throws Exception{
+        ResultSet rset = stmt.executeQuery("Select * From Music");
+        int count = 0;
+        int id;
+        String name, author, year, type;
+        MusicList.clear();
+        MusicList2.clear();
+        while (rset.next()){
+            id = rset.getInt("MusicId");
+            name = rset.getString("MusicName");
+            author = rset.getString("Author");
+            year = rset.getString("Years");
+            type = rset.getString("Types");
+            MusicList.add(new Music(id,name,author,year,type));
+            count++;
+        }
+        System.out.println(count + " ban ghi dong bo thanh cong");
+    }
+    public void UpdateMusic(PreparedStatement psmt) throws Exception{
         System.out.println("= Chinh sua =");
         System.out.println("Nhap id bai hat can chinh sua :");
         int id = scan.nextInt();
         scan.nextLine();
         if (checkId(id)) {
-            try(
-                    Connection conn = DriverManager.getConnection(
-                            "jdbc:mysql://localhost:3306/Music", "root","");
-                    PreparedStatement psmt = conn.prepareStatement("UPDATE Music set Musicname = ?, Author = ?, years = ?, types = ?");
-            ) {
+            for (Music m : MusicList) {
+                if (m.getMusicId() == id) {
+                    System.out.println("Nhap ten bai hat: ");
+                    String name = scan.nextLine();
+                    m.setMusicName(name);
+                    System.out.println("Nhap ten tac gia: ");
+                    String author = scan.nextLine();
+                    m.setAuthor(author);
+                    System.out.println("Nhap nam phat hanh: ");
+                    String year = scan.nextLine();
+                    m.setYears(year);
+                    System.out.println("Nhap the loai: ");
+                    String type = scan.nextLine();
+                    m.setType(type);
+                    System.out.println("Thay doi thanh cong");
 
-                try {
-                    for (Music m : MusicList) {
-                        if (m.getMusicId() == id) {
-                            System.out.println("Nhap ten bai hat: ");
-                            String name = scan.nextLine();
-                            m.setMusicName(name);
-                            System.out.println("Nhap ten tac gia: ");
-                            String author = scan.nextLine();
-                            m.setAuthor(author);
-                            System.out.println("Nhap nam phat hanh: ");
-                            String year = scan.nextLine();
-                            m.setYears(year);
-                            System.out.println("Nhap the loai: ");
-                            String type = scan.nextLine();
-                            m.setType(type);
-                            System.out.println("Thay doi thanh cong");
-
-                            psmt.setString(1,name);
-                            psmt.setString(2,author);
-                            psmt.setString(3,year);
-                            psmt.setString(4,type);
-                            psmt.addBatch();
-                            int[] returnRecord = psmt.executeBatch();
-                            System.out.println("Return codes are: ");
-                            for (int code : returnRecord) System.out.println(code + ", ");
-                            System.out.println();
-                        }
-                    }
-                }catch (SQLException ex){
-                    ex.printStackTrace();
-                    conn.rollback();
-                    System.out.println("Sua doi that bai");
+                    psmt.setString(1,name);
+                    psmt.setString(2,author);
+                    psmt.setString(3,year);
+                    psmt.setString(4,type);
+                    psmt.setInt(5,id);
+                    psmt.addBatch();
+                    int[] returnRecord = psmt.executeBatch();
+                    System.out.println("Return codes are: ");
+                    for (int code : returnRecord) System.out.println(code + ", ");
+                    System.out.println();
                 }
             }
         }else {
@@ -97,31 +113,19 @@ public class MusicList {
 
     }
 
-    public void RemoveMusic() throws Exception{
+    public void RemoveMusic(Statement psmt) throws Exception{
         System.out.println("= Xoa bai hat =");
         System.out.println("Nhap id bai hat can xoa :");
         int id = scan.nextInt();
         scan.nextLine();
         if (checkId(id)) {
-            try(
-                    Connection conn = DriverManager.getConnection(
-                            "jdbc:mysql://localhost:3306/Music", "root","");
-                    PreparedStatement psmt = conn.prepareStatement("DELETE from Music where id = ?");
-            ) {
-                try {
-                    for (Music m : MusicList) {
-                        if (m.getMusicId() == id) {
-                            MusicList.remove(m);
-                            psmt.setInt(1,id);
-                            int count = psmt.executeUpdate();
-                            System.out.println(count + "removed");
-                            System.out.println("Xoa thanh cong !!!");
-                        }
-                    }
-                }catch (SQLException ex){
-                    ex.printStackTrace();
-                    conn.rollback();
-                    System.out.println("Xoa that bai");
+            for (Music m : MusicList) {
+                if (m.getMusicId() == id) {
+                    MusicList.remove(m);
+                    int count = psmt.executeUpdate("DELETE FROM Music WHERE MusicId = " + id);
+                    System.out.println(count + "removed");
+                    System.out.println("Xoa thanh cong !!!");
+                    break;
                 }
             }
         }else {
@@ -129,62 +133,44 @@ public class MusicList {
         }
     }
 
-    public void SearchMusic() throws Exception{
-        System.out.println("= Tim kiem bai hat =");
-        try(
-                Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/Music", "root","");
-                PreparedStatement pstm = conn.prepareStatement("Select * from Music where ? Like '%?%'");
-        ) {
-            try {
-                System.out.println("1. Tim theo ten");
-                System.out.println("2. Tim theo tac gia");
-                System.out.println("3. Tim theo the loai");
-                System.out.println("4. Thoat");
-                System.out.println("Lua chon :");
-                int select = scan.nextInt();
-                scan.nextLine();
-                boolean check = true;
-                do {
-                    switch (select) {
-                        case 1:
-                            System.out.println("Nhap ten bai hat can tim :");
-                            String name = scan.nextLine();
-                            pstm.setString(1,"MusicName");
-                            pstm.setString(2,name);
-                            ResultSet rset = pstm.executeQuery();
-                            DisplayMeta(rset);
-                            break;
-                        case 2:
-                            System.out.println("Nhap ten tac gia can tim :");
-                            String author = scan.nextLine();
-                            pstm.setString(1,"Author");
-                            pstm.setString(2,author);
-                            ResultSet rset2 = pstm.executeQuery();
-                            DisplayMeta(rset2);
-                            break;
-                        case 3:
-                            System.out.println("Nhap ten the loai can tim :");
-                            String type = scan.nextLine();
-                            pstm.setString(1,"Type");
-                            pstm.setString(2,type);
-                            ResultSet rset3 = pstm.executeQuery();
-                            DisplayMeta(rset3);
-                            break;
-                        case 4:
-                            check = false;
-                            break;
-                        default:
-                            System.out.println("Nhap sai lua chon !!!");
-                            break;
-                    }
-                } while (check);
-            }catch (SQLException ex){
-                ex.printStackTrace();
-                conn.rollback();
-                System.out.println("Tim kiem that bai !!!");
+    public void SearchMusic(Statement stmt) throws Exception{
+        boolean check = true;
+        do {
+            System.out.println("= Tim kiem bai hat =");
+            System.out.println("1. Tim theo ten");
+            System.out.println("2. Tim theo tac gia");
+            System.out.println("3. Tim theo the loai");
+            System.out.println("4. Thoat");
+            System.out.println("Lua chon :");
+            int select = scan.nextInt();
+            scan.nextLine();
+            switch (select) {
+                case 1:
+                    System.out.println("Nhap ten bai hat can tim :");
+                    String name = scan.nextLine();
+                    ResultSet rset = stmt.executeQuery("Select * from Music WHERE MusicName Like '%" + name + "%'");
+                    DisplayMeta(rset);
+                    break;
+                case 2:
+                    System.out.println("Nhap ten tac gia can tim :");
+                    String author = scan.nextLine();
+                    ResultSet rset2 = stmt.executeQuery("Select * from Music WHERE Author Like '%" + author + "%'");
+                    DisplayMeta(rset2);
+                    break;
+                case 3:
+                    System.out.println("Nhap ten the loai can tim :");
+                    String type = scan.nextLine();
+                    ResultSet rset3 = stmt.executeQuery("Select * from Music WHERE Types Like '%" + type + "%'");
+                    DisplayMeta(rset3);
+                    break;
+                case 4:
+                    check = false;
+                    break;
+                default:
+                    System.out.println("Nhap sai lua chon !!!");
+                    break;
             }
-        }
+        } while (check);
     }
 
     public void DisplayArrayList() {
@@ -198,91 +184,42 @@ public class MusicList {
     }
 
     public void DisplayMeta(ResultSet rset) throws Exception{
-        ResultSetMetaData rmeta = rset.getMetaData();
+                ResultSetMetaData rmeta = rset.getMetaData();
 
-        int numColumns = rmeta.getColumnCount();
-        System.out.println("= Display Database =");
-        // Columns name
-        for (int i = 1; i <= numColumns; ++i) {
-            System.out.printf("%-30s", "(" + rmeta.getColumnName(i) + ")");
-        }
-        System.out.println();
+                int numColumns = rmeta.getColumnCount();
+                System.out.println("= Display Database =");
+                // Columns name
+                for (int i = 1; i <= numColumns; ++i) {
+                    System.out.printf("%-30s", "(" + rmeta.getColumnName(i) + ")");
+                }
+                System.out.println();
 
-        while (rset.next()) {
-            for (int i = 1; i <= numColumns; ++i) {
-                System.out.printf("%-30s", rset.getString(i));
-            }
-            System.out.println();
-        }
-    }
-
-    public void DisplayDatabase() throws Exception{
-        try(
-                Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/Music", "root","");
-                Statement stmt = conn.createStatement();
-        ) {
-            try {
-                ResultSet rset = stmt.executeQuery("Select * from Music");
-                DisplayMeta(rset);
-            }catch (SQLException ex){
-                ex.printStackTrace();
-                conn.rollback();
-            }
-        }
-    }
-
-    public boolean checkIDDatabase(int id) throws Exception{
-        boolean check = false;
-        try(
-                Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/Music", "root","");
-                Statement stmt = conn.createStatement();
-        ) {
-            try {
-                ResultSet rset = stmt.executeQuery("Select * from Music");
                 while (rset.next()) {
-                    if (rset.getInt("MusicId") == id) {
-                        check = true;
-                        break;
+                    for (int i = 1; i <= numColumns; ++i) {
+                        System.out.printf("%-30s", rset.getString(i));
                     }
+                    System.out.println();
                 }
-            }catch (SQLException ex){
-                ex.printStackTrace();
-                conn.rollback();
-            }
-        }
-        return check;
     }
-    public void SaveDatabase() throws Exception{
-        try(
-                Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/Music", "root","");
-                PreparedStatement pstmt = conn.prepareStatement("Insert into Music values (?,?,?,?,?)")
-        ) {
-            try {
-                int count = 0;
-                int fail = 0;
-                for (Music m : MusicList) {
-                    if (checkIDDatabase(m.getMusicId())){
-                        fail++;
-                    }else {
-                        pstmt.setInt(1, m.getMusicId());
-                        pstmt.setString(2, m.getMusicName());
-                        pstmt.setString(3, m.getAuthor());
-                        pstmt.setString(4, m.getYears());
-                        pstmt.setString(5,m.getType());
-                        pstmt.addBatch();
-                        int[] returnRecord = pstmt.executeBatch();
-                        count++;
-                    }
-                }
-                System.out.println( count + " Record inserted");
-                System.out.println( fail + " Record insert fail");
-            }catch (SQLException ex){
-                ex.printStackTrace();
-                conn.rollback();
-            }
+
+    public void DisplayDatabase(Statement stmt) throws Exception{
+        ResultSet rset = stmt.executeQuery("Select * from Music");
+        DisplayMeta(rset);
+    }
+
+    public void SaveDatabase(PreparedStatement pstmt, Statement stmt) throws Exception{
+        int count = 0;
+        for (Music m : MusicList2) {
+            pstmt.setInt(1, m.getMusicId());
+            pstmt.setString(2, m.getMusicName());
+            pstmt.setString(3, m.getAuthor());
+            pstmt.setString(4, m.getYears());
+            pstmt.setString(5,m.getType());
+            pstmt.addBatch();
+            int[] returnRecord = pstmt.executeBatch();
+            count++;
         }
+        System.out.println( count + " Record inserted");
+        Synchronized(stmt);
     }
 }
